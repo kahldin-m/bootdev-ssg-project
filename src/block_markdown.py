8b388fa6fdbf2f455640d1b0e66ea695298d287f
kahldin-m <mglen.413@workmail.com>
2025-12-30 12:10:21 -0900

    Make node now just need to split lines

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,8 +1,9 @@
 ## Helpers for parsing text into block markdown
 import re
 from enum import Enum
-from textnode import TextNode, text_node_to_html_node
-import inline_markdown as im
+from textnode import TextNode, text_node_to_html_node, TextType
+from htmlnode import HTMLNode, ParentNode, LeafNode
+import inline_markdown as imd
 
 
 class BlockType(Enum):
@@ -66,46 +67,94 @@ def block_to_block_type(block):
 ##  That one parent node should contain many child HTMLNode objects representing
 ##      the nested elements
 def markdown_to_html_node(markdown):
+    div_node = ParentNode("div", [], None)
     # Split md into blocks
     blocks = markdown_to_blocks(markdown)
-
-    # Loop over each block
-    block_types = []
-
     for block in blocks:
         # Determing block type
-        fix_nodes = text_to_children(block)
-        block_types.append(block_to_block_type(fix_nodes))
-        
-    return block_types
+        typed_block = block_to_block_type(block)
+        if typed_block == BlockType.HEADING:
+            hashes = 0
+            for hash in block:
+                if hash == "#":
+                    hashes += 1
+                else:
+                    break
+            clean_text = block.strip("#")
+            new_block_children = text_to_children(clean_text.strip())
+            div_node.children.append(ParentNode(f"h{hashes}", new_block_children, None))
+        elif typed_block == BlockType.PARAGRAPH:
+            new_block_children = text_to_children(block)
+            div_node.children.append(ParentNode("p", new_block_children, None))
+        elif typed_block == BlockType.CODE:
+            clean_text = block.strip("```")
+            clean_text_two = clean_text.rstrip("```")
+            code_node = text_node_to_html_node(TextNode(clean_text_two.strip(), TextType.CODE))
+            div_node.children.append(ParentNode("pre", [code_node]))
+        elif typed_block == BlockType.QUOTE:
+            clean_text = block.lstrip(">")
+            new_block_children = text_to_children(clean_text)
+            div_node.children.append(ParentNode("blockquote", new_block_children, None))
+        elif typed_block == BlockType.ULIST:
+            clean_text = block.lstrip("-")
+            new_block_children = text_to_children(clean_text.strip())
+            div_node.children.append(ParentNode("ul", new_block_children, None))
+        elif typed_block == BlockType.OLIST:
+            clean_text = block.lstrip("0123456789.")
+            new_block_children = text_to_children(clean_text.strip())
+            div_node.children.append(ParentNode("ol", new_block_children, None))
+        else:
+            raise Exception("Duh, something went wrong.")
+    
 
-## helper function
+    # GOAL: Return an HTMLNode for the whole document: a Parent <div> node whose children
+    # are block-level nodes (like <p>, <h1>, <ul>, etc.)
+    # Each paragraph block becomes one <p> node whose children are inline LeafNodes
+    # created from the paragraph’s text.
+    return div_node
+
+### helper functions
 def text_to_children(text):
-    print(f"text {text} -> {type(text)}")
-    text_nodes = im.text_to_textnodes(text)
-    print(f"text {text_nodes} -> {type(text_nodes)}")
-    for node in text_nodes:
-        new_node = text_node_to_html_node(node)
-        print(new_node)
-
-
+    # Take a string of text and return a list of
+    # HTMLNodes that represent the inline markdown
+    # using TextNode -> HTMLNode
+    print(f">>TEXT to Child == {text} <<<")
+    new_texts = imd.text_to_textnodes(text)
+    text_nodes = []
+    for new in new_texts:
+        text_nodes.append(text_node_to_html_node(new))
+    return text_nodes
 
 
 if __name__ == "__main__":
 ## Block to HTMLNode test
     md = """
-# A heading title
+### A heading title
 
 This is **bolded** paragraph
 text in a p
 tag here
 
-This is another paragraph with _italic_ text and `code` here
+This is another paragraph with _italic_ text
 
+```And a code block```
+
+- unordered
+- list items
+- item third
+
+1. ordered
+2. list
+3. items
+
+Plus two [link](www.site.com) ![img](www.image.com)
+
+>And a quote from your future self
 """
-    blocks = markdown_to_html_node(md)
-    for block in blocks:
-        print(block)
+    node = markdown_to_html_node(md)
+    html = node.to_html()
+    print(html)
+    
 
 ## Block Type test
 #     md = """
